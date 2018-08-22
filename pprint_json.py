df43c86dde62ade063d5476588c76617e87d9279
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-22 10:32:42 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,37 @@
 import json
+import argparse
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Path to file with json data')
+    return parser.parse_args()
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath) as file:
+            content = file.read()
+
+        return json.loads(content)
+    except (FileNotFoundError, json.decoder.JSONDecodeError, TypeError):
+        return None
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_data):
+    return json.dumps(json_data, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    args = parse_args()
+
+    filepath = args.file
+    json_data = load_data(filepath)
+
+    if json_data is None:
+        sys.exit('Failed to open json file (not found or incorrect format)')
+
+    prettified_json = pretty_print_json(json_data)
+
+    print(prettified_json)
